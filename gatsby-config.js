52f6c8de54987675438fa0fc6880b37cf6142a39
udapon <udap00n@gmail.com>
2020-04-25 21:06:39 +0900

    change config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   siteMetadata: {
-    title: `Creative Portfolio`,
+    title: `Tatsuya Uda`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
